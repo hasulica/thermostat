d8c6bf7479f849b6c4f53b25c8cb07fec42e116f
hasulica <raduasandei@gmail.com>
2015-11-11 16:05:14 +0000

    Initial commit

---
+++ spec/featureSpec.js
@@ -0,0 +1,27 @@
+describe('Feature Test:', function(){
+  var plane;
+  var airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it('planes can be instructed to land at an airport', function(){
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport);
+    plane.takeOff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
+  it('blocks takeoff when weather is stormy', function(){
+    plane.land(airport);
+    spyOn(airport, 'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeOff(); }).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
+    });
+});
