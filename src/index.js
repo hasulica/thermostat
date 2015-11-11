41e37080fe1aeb4fb05d5d160b4ad2c3dc4c7c78
hasulica <raduasandei@gmail.com>
2015-11-11 18:06:22 +0000

    Finished JQuery script;

---
+++ src/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+
+  function updateTemperature() {
+    $('.temperature').text(thermostat.getCurrentTemperature());
+    $('.temperature').addClass(thermostat.getUsage());
+  }
+
+  updateTemperature();
+
+  $('.up').on('click', function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('.down').on('click', function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('.reset').on('click', function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('.PSM-on').on('click', function() {
+    thermostat.turnPowerSavingOn();
+    updateTemperature();
+  });
+
+  $('.PSM-off').on('click', function() {
+    thermostat.turnPowerSavingOff();
+    updateTemperature();
+  });
+
+});
