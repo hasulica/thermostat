a8e3dfc8a0dc700baf7434f879544ffed9a56ff9
hasulica <raduasandei@gmail.com>
2015-11-13 14:02:11 +0000

    search

--- src/search.js
+++ src/search.js
@@ -1,20 +1,39 @@
 $(document).ready(function() {
-  $('button').click(function() {
-    var query = $('.search input').val();
-    $.ajax({
-      url : "http://autocomplete.wunderground.com/aq?query=" + query,
-      dataType : "jsonp",
-      jsonp: "callback",
-      jsonpCallback: "jsonpCallbackfunction"
+    $('button').click(function() {
+        var query = $('.search input').val();
+        var url = 'http://api.openweathermap.org/data/2.5/find?q=' + query + '&type=like&mode=json&appid=2de143494c0b295cca9337e1e96b00e0';
+        $.ajax({
+          url : url,
+          dataType : "jsonp",
+          success : function(data) {
+            data.list.forEach( function(object) {
+              $('.results').append("<p id=" + object.id + ">" + object.name + "</p>");
+            });
+          }
+        });
     });
 
-    function jsonpCallbackfunction(json) {
-      $.each(data.Results,function() {
-        var city = this.Name;
-        $('.results').append('<p>' + city + '</p>');
-      });
-    }
 
-  });
 
+    function ajaxRequest(evt) {
+          var objectId = evt.attr('id');
+          event.preventDefault();
+          $.ajax({
+            url : "http://api.openweathermap.org/data/2.5/weather?id=" + objectId + "&appid=2de143494c0b295cca9337e1e96b00e0",
+            dataType : "jsonp",
+            success : function(data) {
+              var json = data;
+              var location = data['name'];
+              var temp_c = data['main']['temp'];
+              var weather = data['weather'][0]['description'];
+              $(".current-weather").html(location + " is " + weather + " and the temperature is " + temp_c + '&#176;');
+            }
+          });
+        }
+
+
+});
+
+$(document).on('click', '.result p', function(){
+  ajaxRequest(this);
 });
--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $.ajax({
-    url : "http://api.wunderground.com/api/380665aee49bae41/geolookup/conditions/q/UK/London.json",
+    url : "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0",
     dataType : "jsonp",
-    success : function(parsed_json) {
-      var location = parsed_json['location']['city'];
-      var temp_c = parsed_json['current_observation']['temp_c'];
-      var weather = parsed_json['current_observation']['weather'];
+    success : function(data) {
+      var json = data;
+      var location = data['name'];
+      var temp_c = data['main']['temp'];
+      var weather = data['weather'][0]['description'];
       $(".current-weather").html(location + " is " + weather + " and the temperature is " + temp_c + '&#176;');
     }
   });
