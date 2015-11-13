76828874650bf905bd414176a4f5f189e6fb4f0c
hasulica <raduasandei@gmail.com>
2015-11-13 14:26:43 +0000

    Added dynamic city results

--- src/search.js
+++ src/search.js
@@ -13,27 +13,21 @@ $(document).ready(function() {
         });
     });
 
-
-
-    function ajaxRequest(evt) {
-          var objectId = evt.attr('id');
-          event.preventDefault();
-          $.ajax({
-            url : "http://api.openweathermap.org/data/2.5/weather?id=" + objectId + "&appid=2de143494c0b295cca9337e1e96b00e0",
-            dataType : "jsonp",
-            success : function(data) {
-              var json = data;
-              var location = data['name'];
-              var temp_c = data['main']['temp'];
-              var weather = data['weather'][0]['description'];
-              $(".current-weather").html(location + " is " + weather + " and the temperature is " + temp_c + '&#176;');
-            }
-          });
-        }
-
-
-});
-
-$(document).on('click', '.result p', function(){
-  ajaxRequest(this);
+    $('.results').on('click', function(event) {
+      if (event.target.tagName === 'P') {
+        var objectId = event.target.attributes.getNamedItem('id').value;
+        event.preventDefault();
+        $.ajax({
+          url : "http://api.openweathermap.org/data/2.5/weather?id=" + objectId + "&appid=2de143494c0b295cca9337e1e96b00e0",
+          dataType : "jsonp",
+          success : function(data) {
+            var json = data;
+            var location = data['name'];
+            var temp_c = data['main']['temp'];
+            var weather = data['weather'][0]['description'];
+            $(".current-weather").html(location + " is " + weather + " and the temperature is " + temp_c + '&#176;');
+          }
+        });
+      }
+    });
 });
